007b84faf9a0c1d654b356ab0f41a5cf259dc8d1
Riko <riko.fluchel@gmail.com>
2025-04-15 07:29:45 -0400

    complete code along

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -7,5 +7,12 @@ describe("pointsForWord", () => {
         const points = pointsForWord(word);
         expect(points).toBe(7);
     });
+
+    it("handles uppercae and lowercase input", () => {
+        const word = "tEsT";
+        const points = pointsForWord(word);
+
+        expect(points).toBe(7);
+    })
 });
 
